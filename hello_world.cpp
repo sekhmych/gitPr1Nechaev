33317fb54b1895b3b2edb03d63b237927c123fca
sekhmych <sekhmych@gmail.com>
2024-09-30 03:33:13 +0000

    Changed hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 int main() {
-
-	cout << "Hello, World!" << endl;
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Hello world from " << name << endl;
 	return 0;
 }
